942f03c47d7d852ff2236f74bd9002a05003be6b
EdwardMudaida <eddieigo8@gmail.com>
2017-07-03 17:36:17 +0300

    Add test for creating a new Parcel

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -2,11 +2,10 @@ require('rspec')
 require('parcel')
 
 describe(Parcel) do
-  describe("#create") do
+  describe("#create?") do
     it('Creates a new parcel') do
-      my_parcel = Parcel.new()
-      my_parcel.create('20', '20', '20', '100')
-      expect(my_parcel.create()).to(eq('20', '20', '20', '100'))
+      my_parcel = Parcel.new('20', '20', '20', '100')
+      expect(my_parcel.create?).to(eq(true))
     end
   end
 end
