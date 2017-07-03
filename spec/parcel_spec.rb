f40264d8e3a5fcc32b3e221ac3c57dc879c461d7
EdwardMudaida <eddieigo8@gmail.com>
2017-07-03 17:47:45 +0300

    Add test for volume

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -8,4 +8,11 @@ describe(Parcel) do
       expect(my_parcel.create?).to(eq(true))
     end
   end
+
+  describe("#volume") do
+    it('Returns the product of the sides.') do
+      my_parcel = Parcel.new('20', '20', '20', '100')
+      expect(my_parcel.volume()).to(eq(8000))
+    end
+  end
 end
