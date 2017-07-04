75502933ef6fdf6adcbba412dcc8a412c2e4678d
EdwardMudaida <eddieigo8@gmail.com>
2017-07-04 05:18:06 +0300

    Display properties of a new instance of a Parcel class & shipping cost

---
+++ app.rb
@@ -0,0 +1,14 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/parcel')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get ('/index') do
+  @parcel = Parcel.new(params.fetch('height'), params.fetch('width'), params.fetch('length'), params.fetch('weight'))
+  @cost = @parcel.cost_to_ship()
+  erb(:index)
+end
