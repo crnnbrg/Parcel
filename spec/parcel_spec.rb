dced861b8ada5392c78953d5e0085277c2c86861
EdwardMudaida <eddieigo8@gmail.com>
2017-07-04 05:18:56 +0300

    Add tests for display properties of a new instance of a Parcel class

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -15,4 +15,42 @@ describe(Parcel) do
       expect(my_parcel.volume()).to(eq(8000))
     end
   end
+
+  describe("#cost_to_ship") do
+    it("Calculates the cost of shipping") do
+      my_parcel = Parcel.new('20', '20', '20', '100')
+      expect(my_parcel.cost_to_ship()).to(eq(1500))
+    end
+  end
+
+  describe("#width") do
+    it('returns the width of a parcel') do
+      my_parcel = Parcel.new('20', '20', '20', '100')
+      expect(my_parcel.width()).to(eq("20"))
+    end
+  end
+
+
+  describe("#height") do
+    it('returns the height of a parcel') do
+      my_parcel = Parcel.new('20', '20', '20', '100')
+      expect(my_parcel.height()).to(eq("20"))
+    end
+  end
+
+
+  describe("#length") do
+    it('returns the length of a parcel') do
+      my_parcel = Parcel.new('20', '20', '20', '100')
+      expect(my_parcel.length()).to(eq("20"))
+    end
+  end
+
+
+  describe("#weight") do
+    it('returns the weight of a parcel') do
+      my_parcel = Parcel.new('20', '20', '20', '100')
+      expect(my_parcel.weight()).to(eq("100"))
+    end
+  end
 end
